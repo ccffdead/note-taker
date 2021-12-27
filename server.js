772b0e8678d4084b3f93bac90a90257f1f2a2496
ccffdead <ccffdead@gmail.com>
2021-12-26 21:45:48 -0500

    added API routes to notes/index.html files

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//global config
+
 const PORT = process.env.PORT || 3001;
 const path = require("path");
 const fs = require("fs");
@@ -8,3 +10,30 @@ const notesData = require("./db/db.json");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+// Add API Route
+
+app.get("/api/notes", (req, res) => {
+  res.json(notesData.slice(1));
+});
+
+//Notes API Route
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+// Index API Routes
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+//Add func to establish new note
+
+//function addnewNote(body, array) {
+//const newNote = body;

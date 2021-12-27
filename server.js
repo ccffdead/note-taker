c7984ae2865e89ea787ffe81021f4cfb5aa35858
ccffdead <ccffdead@gmail.com>
2021-12-26 22:59:59 -0500

    changed file dir

--- server.js
+++ server.js
@@ -27,15 +27,15 @@ app.post("/api/notes", (req, res) => {
 });
 
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
+  res.sendFile(path.join(__dirname, "./notes.html"));
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
+  res.sendFile(path.join(__dirname, "./index.html"));
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
+  res.sendFile(path.join(__dirname, "./index.html"));
 });
 
 //Listening to Port

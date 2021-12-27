93ef1dd927e69a5ea3736b01a89ff4080f273d7e
ccffdead <ccffdead@gmail.com>
2021-12-26 22:45:47 -0500

    final commits

--- server.js
+++ server.js
@@ -1,39 +1,34 @@
 //global config
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5500;
 const path = require("path");
 const fs = require("fs");
 const express = require("express");
 const app = express();
-const notesData = require("./db/db.json");
+let notesInfo = require("./db/db.json");
+const { v4: uuidv4 } = require("uuid");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// Get API notes route
+//API Routes
 
 app.get("/api/notes", (req, res) => {
-  let data = notesData;
+  let data = notesInfo;
   res.json(data);
 });
 
-// Post to notes
-
 app.post("/api/notes", (req, res) => {
   req.body.id = uuidv4();
-  notesData.push(req.body);
-  fs.writeFileSync("./db/db.json", JSON.stringify(notesData, null, "/t"));
-  res.json(notesData);
+  notesInfo.push(req.body);
+  fs.writeFileSync("./db/db.json", JSON.stringify(notesInfo, null, "/t"));
+  res.json(notesInfo);
 });
-
-// Send to notes.html
 
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
-
-// Send to index.html
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
@@ -43,7 +38,7 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-// Listens to PORT and logs in using server number
+//Listening to Port
 
 app.listen(PORT, () => {
   console.log(`Server is now live on port ${PORT}!`);
